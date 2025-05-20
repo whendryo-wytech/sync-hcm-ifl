78181661c9d40771f31771c17c8ae2a5128f6e64
Whendryo <whendryo@wytech.com.br>
2025-05-20 17:53:48 -0300

    Refactor REPs operation

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
 //Schedule::call(static function () {
@@ -8,3 +9,16 @@ use Illuminate\Support\Facades\Schedule;
 
 //Schedule::command('rep:pendency')->everyTwoMinutes()->withoutOverlapping();
 Schedule::command('sync:run')->dailyAt('22:00');
+
+
+Schedule::call(static function () {
+    Artisan::call('rep:clear', [
+        '--devices' => env('DEVICE_MASTER_REP'),
+    ]);
+    Artisan::call('rep:reload');
+    Artisan::call('rep:send', [
+        '--devices'   => env('DEVICE_MASTER_REP'),
+        '--with-slow' => true,
+    ]);
+    Artisan::call('rep:master');
+})->dailyAt('20:00');
